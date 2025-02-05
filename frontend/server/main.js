4adb7a21e84d9a345661da7561cf138361935b27
Priyanshu <priyanshutripathi7777@gmail.com>
2025-02-05 11:37:53 -0500

    refactor: port name in frontend

--- frontend/server/main.js
+++ frontend/server/main.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT;
+const FRONTEND_PORT = process.env.FRONTEND_PORT;
 
 // middleware
 app.use(express.static("public"));
 
 // listening
-app.listen(PORT, function () {
-    console.log(`App started on port http://localhost:${PORT}`);
+app.listen(FRONTEND_PORT, function () {
+    console.log(`App started on port http://localhost:${FRONTEND_PORT}`);
 });
